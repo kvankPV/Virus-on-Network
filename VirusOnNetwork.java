ec2bdf0d6691a246827f71f6f31b6f098be79936
kvankPV <quang.pviet@proton.me>
2022-11-16 10:34:49 +0100

    VirusOnNetwork hlavna trieda

---
+++ VirusOnNetwork.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class VirusOnNetwork here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VirusOnNetwork {
+    /**
+     * Constructor for objects of class VirusOnNetwork
+     */
+    public VirusOnNetwork() {
+        // initialise instance variables
+    }
+}
