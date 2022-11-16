63302bfe947dc2ec069ef7db77462f93bbf6b96a
kvankPV <quang.pviet@proton.me>
2022-11-16 11:10:51 +0100

    moja java

---
+++ VirusOnNet.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class VirusOnNet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VirusOnNet {
+    /**
+     * Constructor for objects of class VirusOnNet
+     */
+    public VirusOnNet() {
+        // initialise instance variables
+    }
+}
