69f5532c4768941c1cdfe9fd304c40d09adf30ee
Dominik Nyc <nyc@stud.uniza.sk>
2022-11-23 10:11:46 +0100

    Trieda Gulicky

---
+++ Gulicky.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Gulicky here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Gulicky {
+    /**
+     * Constructor for objects of class Gulicky
+     */
+    public Gulicky() {
+        // initialise instance variables
+    }
+}
