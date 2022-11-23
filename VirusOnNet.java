b5120aa38887067412fc314ee77f05602a1b9cd0
kvankPV <quang.pviet@proton.me>
2022-11-23 11:10:08 +0100

    oprava

--- VirusOnNet.java
+++ VirusOnNet.java
@@ -1,4 +1,4 @@
-
+import java.util.concurrent.TimeUnit;
 /**
  * Write a description of class VirusOnNet here.
  * 
@@ -9,7 +9,24 @@ public class VirusOnNet {
     /**
      * Constructor for objects of class VirusOnNet
      */
+    private Parametre parametre;
+    private Siet siet;
     public VirusOnNet() {
         // initialise instance variables
+    }
+
+    public void start(int mnozstvoModul, int vzdiaModul, int pocetInfik, double sancaSirenia, double sancaZotavenia, double sancaOdolnosti) {
+        //gettery parametrov
+        Parametre inip = new Parametre(mnozstvoModul, vzdiaModul, pocetInfik, sancaSirenia, sancaZotavenia, sancaOdolnosti);
+        //gettery parametrov
+        Siet inig = new Siet(mnozstvoModul, vzdiaModul, pocetInfik, sancaSirenia, sancaZotavenia, sancaOdolnosti);
+        while (inig.pocetSedych() <= 50) {
+            //siet. a nejaky random sa musi urobit v tej triede
+            Platno.dajPlatno().wait(350);
+        }
+    }
+    
+    public void reset() {
+        //v sieti zavolam nejaky reset ?
     }
 }
