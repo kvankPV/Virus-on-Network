eaca7565faafde1d187261342a22936d8f58552c
yxcvbnm123 <117819081+yxcvbnm123@users.noreply.github.com>
2022-12-05 21:15:00 +0100

    Add files via upload
    Hotovo

---
+++ siet.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.scene.shape.Line;
+public class siet {
+    private int sirka;
+    private Obdlznik obdlznik;
+    private Random random;
+    private VelaGuliciek gulicky;
+    private ArrayList <VelaGuliciek> s1;
+    private Line line;
+    public siet() {
+        
+        
+    }
+    
+    public void spojGulicky(){
+    
+    }
+    
+    public void resetSiete(){
+        this.gulicky.vymazVsetkyGulicky();
+        
+    }
+}
